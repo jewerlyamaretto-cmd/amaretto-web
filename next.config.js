f7f436eaa4358210c4f3fc7a77cb56a7a3518fbc
kevin dominguez <kevindomp@iMac-de-kevin.local>
2026-01-05 14:20:27 -0600

    Fix Next.js Image config: allow Cloudinary images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+    // Permitir imágenes sin optimización si es necesario
+    unoptimized: false,
+  },
 }
 
 module.exports = nextConfig
